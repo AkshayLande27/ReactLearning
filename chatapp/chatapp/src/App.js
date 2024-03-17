6b75ad12c580b164f2fa7a9568576af1b5c13d8e
sarcasticakkii <120239176+sarcasticakkii@users.noreply.github.com>
2024-03-17 22:55:09 +0530

    Initial commit: added project files

---
+++ chatapp/chatapp/src/App.js
@@ -0,0 +1,15 @@
+import {Chatengine} from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <Chatengine
+            height="100vh"
+            projectID="40ba72ec-9ad2-456b-9b5d-992ca5306896"
+            userSecret=""
+        />
+    );
+
+    export default App;
+}
